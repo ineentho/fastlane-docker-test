0a546359a9bfc6fe71012be8e9311635e663c729
Henrik Karlsson <hk.henrik@gmail.com>
2019-05-11 16:04:41 +0200

    Initial Commit

---
+++ fastlane-docker-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fastlane-docker-test
+//
+//  Created by Henrik Karlsson on 2019-05-11.
+//  Copyright © 2019 Henrik Karlsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
